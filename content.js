ca4fd5ded976aa062ad45922f49196f3eac31c90
Ryan Troyer <troyerrdt@gmail.com>
2015-09-04 14:51:05 -0400

    Updated for changes to Amazon site, multiple authors, subtitles, fix for multiple tabs
    Updated the secondary author selector.
    Allowed for multiple authors in selector and search.
    Parsed out subtitle for better search results.
    Fixed issue I was having where subsequent clicks to the icon open
    multiple tabs, one for each previous click of the icon.

--- content.js
+++ content.js
@@ -1,15 +1,27 @@
 var titleNode = document.querySelector('#productTitle');
 var authorNode = document.querySelector('.contributorNameID');
 
+var authorNodes = document.querySelectorAll('.contributorNameID');
+
+if (authorNodes == null) {
+    authorNodes = document.querySelectorAll('.author a');
+}
+
+var authorList = [];
+for (var i = 0; i < authorNodes.length; i++) {
+    var author = authorNodes[i].textContent.toUpperCase();
+    authorList[i] = author;
+}
+
 if (!titleNode) {
-    titleNode = document.querySelector('.kindleBanner i')
+    titleNode = document.querySelector('.kindleBanner i');
 }
 
 if (!authorNode) {
-    authorNode = document.querySelector('.contributorNameTrigger a')
+    authorNode = document.querySelector('.author a');
 }
 
 var title = titleNode.textContent;
 var author = authorNode.textContent;
 
-chrome.runtime.sendMessage({data: {author: author, title: title}}, function(response) {});
+chrome.runtime.sendMessage({data: {author: author, title: title, authorList: authorList}}, function(response) {});
